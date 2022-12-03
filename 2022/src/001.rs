354c6180253c2a102ba6803849278cab89605ce5
Chris Wren <chris@drwren.com>
2022-12-03 10:13:31 -0500

    add some tests

--- 2022/src/001.rs
+++ 2022/src/001.rs
@@ -1,6 +1,7 @@
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
+use std::borrow::Borrow;
 
 #[derive(Debug)]
 struct Elf {
@@ -23,14 +24,28 @@ fn main() {
     let f = File::open("input/001.txt")
         .expect("File Error");
     let reader = BufReader::new(f);
+    let lines = reader.lines().flatten();
+    let elves = parse_elves(lines);
+    let mut inventory: Vec<u32> = elves.iter().map(|elf| { elf.total_snacks() }).collect();
+    println!("max is {:?}", inventory.iter().max());
+
+    inventory.sort();
+    inventory.reverse();
+    println!("top 3 is {:?}", inventory[0..3].iter().sum::<u32>());
+}
+
+fn parse_elves<I>(lines: I) -> Vec<Elf> 
+where
+    I: IntoIterator,
+    I::Item: Borrow<str>,{
     let mut elves = Vec::new();
     let mut elf = new_elf();
-    for snack in reader.lines().flatten() {
-        if snack.is_empty() {
+    for snack in lines {
+        if snack.borrow().is_empty() {
             elves.push(elf);
             elf = new_elf();
         } else {
-            let calories: u32 = snack.trim().parse()
+            let calories: u32 = snack.borrow().trim().parse()
                 .expect("Not a Number");
             elf.snacks.push(calories);
         }
@@ -38,18 +53,36 @@ fn main() {
     if !elf.snacks.is_empty() {
         elves.push(elf);
     }
-    let mut inventory: Vec<u32> = elves.iter().map(|elf| { elf.total_snacks() }).collect();
-    println!("max is {:?}", inventory.iter().max());
-
-    inventory.sort();
-    inventory.reverse();
-    println!("top 3 is {:?}", inventory[0..3].iter().sum::<u32>());
+    elves
 }   
 
 #[cfg(test)]
 mod tests {
+    use crate::parse_elves;
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
+    fn test_parse() {
+        let elves = parse_elves(Vec::from([
+            "10",
+            "11",
+            "",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "",
+            "30",
+            "31",
+            "32",
+            "33",
+            "",
+            "44"]).into_iter()
+        );
+        assert_eq!(elves.len(), 4);
+        assert_eq!(elves[0].snacks.len(), 2);
+        assert_eq!(elves[1].snacks.iter().max(), Some(&25));
+        assert_eq!(elves[2].snacks.iter().sum::<u32>(), 30 + 31 + 32 + 33);
+        assert_eq!(elves[3].snacks.len(), 1);
     }
 }
