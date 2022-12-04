1caa21a3c146f323146059e7866da4c22392b3c5
Chris Wren <chris@drwren.com>
2022-12-04 17:42:34 -0500

    day 4 rustfmt

--- 2022/src/004.rs
+++ 2022/src/004.rs
@@ -1,5 +1,5 @@
-use std::{fs::File, io::Read};
 use regex::Regex;
+use std::{fs::File, io::Read};
 
 const ELF_PAIR_RE: &str = r"^(\d+)-(\d+),(\d+)-(\d+)$";
 
@@ -11,13 +11,11 @@ fn contained_in(a: &Job, b: &Job) -> bool {
 }
 
 fn overlapping(a: &Job, b: &Job) -> bool {
-    (a.0 <= b.0 && a.1 >= b.0) ||
-    (a.0 <= b.1 && a.1 >= b.1) ||
-    (a.0 <= b.1 && a.1 >= b.0) 
+    (a.0 <= b.0 && a.1 >= b.0) || (a.0 <= b.1 && a.1 >= b.1) || (a.0 <= b.1 && a.1 >= b.0)
 }
 
 fn parse_jobs(input: &str) -> Vec<ElfPair> {
-    let elf_pair_re: regex::Regex= Regex::new(ELF_PAIR_RE).unwrap();
+    let elf_pair_re: regex::Regex = Regex::new(ELF_PAIR_RE).unwrap();
     let mut jobs = Vec::new();
 
     for line in input.lines() {
@@ -40,17 +38,19 @@ fn main() {
     let mut f = File::open("input/004.txt").expect("File Error");
     let mut input = String::new();
     f.read_to_string(&mut input).expect("File Read Error");
-    
+
     let jobs = parse_jobs(&input);
-    let count = jobs.iter()
+    let count = jobs
+        .iter()
         .map(|j| contained_in(&j.0, &j.1) || contained_in(&j.1, &j.0))
-        .filter(|b|*b)
+        .filter(|b| *b)
         .count();
     println!("contains: {:?}", count);
 
-    let count = jobs.iter()
+    let count = jobs
+        .iter()
         .map(|j| overlapping(&j.0, &j.1))
-        .filter(|b|*b)
+        .filter(|b| *b)
         .count();
     println!("overlaps: {:?}", count);
 }
@@ -69,14 +69,14 @@ mod tests {
     fn test_parse() {
         let jobs = parse_jobs(SAMPLE);
         assert_eq!(jobs.len(), 6);
-        assert_eq!(jobs[0].0.0, 2);
-        assert_eq!(jobs[0].0.1, 4);
-        assert_eq!(jobs[0].1.0, 6);
-        assert_eq!(jobs[0].1.1, 8);
-        assert_eq!(jobs[4].0.0, 6);
-        assert_eq!(jobs[4].0.1, 6);
-        assert_eq!(jobs[4].1.0, 4);
-        assert_eq!(jobs[4].1.1, 6);
+        assert_eq!(jobs[0].0 .0, 2);
+        assert_eq!(jobs[0].0 .1, 4);
+        assert_eq!(jobs[0].1 .0, 6);
+        assert_eq!(jobs[0].1 .1, 8);
+        assert_eq!(jobs[4].0 .0, 6);
+        assert_eq!(jobs[4].0 .1, 6);
+        assert_eq!(jobs[4].1 .0, 4);
+        assert_eq!(jobs[4].1 .1, 6);
     }
 
     #[test]
@@ -90,11 +90,11 @@ mod tests {
     #[test]
     fn test_count_contains() {
         let jobs = parse_jobs(SAMPLE);
-        let count = jobs.iter()
+        let count = jobs
+            .iter()
             .map(|j| contained_in(&j.0, &j.1) || contained_in(&j.1, &j.0))
-            .filter(|b|*b)
-            .collect::<Vec<bool>>()
-            .len();
+            .filter(|b| *b)
+            .count();
         assert_eq!(count, 2);
     }
 
@@ -112,24 +112,24 @@ mod tests {
     #[test]
     fn test_count_overlaps() {
         let jobs = parse_jobs(SAMPLE);
-        let count = jobs.iter()
+        let count = jobs
+            .iter()
             .map(|j| overlapping(&j.0, &j.1))
-            .filter(|b|*b)
-            .collect::<Vec<bool>>()
-            .len();
+            .filter(|b| *b)
+            .count();
         assert_eq!(count, 4);
     }
 
     #[test]
     fn test_regex_match() {
-        let elf_pair_re: regex::Regex= Regex::new(ELF_PAIR_RE).unwrap();
+        let elf_pair_re: regex::Regex = Regex::new(ELF_PAIR_RE).unwrap();
         assert!(elf_pair_re.is_match("5-7,6-9"));
         assert!(elf_pair_re.is_match("522-794,62302-954322"));
     }
 
     #[test]
     fn test_regex_groups() {
-        let elf_pair_re: regex::Regex= Regex::new(ELF_PAIR_RE).unwrap();
+        let elf_pair_re: regex::Regex = Regex::new(ELF_PAIR_RE).unwrap();
         let caps = elf_pair_re.captures("54-78,62-91").unwrap();
         assert_eq!(caps.get(1).unwrap().as_str().parse::<u32>().unwrap(), 54);
         assert_eq!(caps.get(2).unwrap().as_str().parse::<u32>().unwrap(), 78);
