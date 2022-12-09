853e595a4358dea0fdf21200112fcfecf51f687b
Chris Wren <chris@drwren.com>
2022-12-08 20:46:21 -0500

    day 8 part 2

--- 2022/src/008.rs
+++ 2022/src/008.rs
@@ -77,12 +77,89 @@ fn visible(orchard: &Vec<Vec<i8>>) -> usize {
     total
 }
 
+fn score_location(orchard: &Vec<Vec<i8>>, y: usize, x:usize) -> u32 {
+    let h = orchard.len();
+    let w = orchard[0].len();
+    let mut scores: Vec<u32> = Vec::new();
+
+    let tree = orchard[y][x];
+    let i = y;
+    let mut score = 0;
+    let mut j = x + 1;
+    while j < w {
+        score += 1;
+        if tree <= orchard[i][j] {
+            break;
+        }
+        j += 1;
+    }
+    scores.push(score);
+    let mut score = 0;
+    if x > 0 {
+        let mut j = x - 1;
+        loop {
+            score += 1;
+            if tree <= orchard[i][j] {
+                break;
+            }
+            if j == 0 {
+                break
+            }
+            j -= 1;
+        }
+    }
+    scores.push(score);
+
+    let j = x;
+    let mut score = 0;
+    let mut i = y + 1;
+    while i < h {
+        score += 1;
+        if tree <= orchard[i][j] {
+            break;
+        }
+        i += 1;
+    }
+    scores.push(score);
+    let mut score = 0;
+    if y > 0 {
+        let mut i = y - 1;
+        loop {
+            score += 1;
+            if tree <= orchard[i][j] {
+                break;
+            }
+            if i == 0 {
+                break
+            }
+            i -= 1;
+        }
+    }
+    scores.push(score);
+
+    scores.iter().product()
+}
+
+
+fn best_view(orchard: &Vec<Vec<i8>>) -> u32 {
+    let mut max = 0;
+    for (i, row) in orchard.iter().enumerate() {
+        for (j, _) in row.iter().enumerate() {
+            let score =score_location(orchard, i, j);
+            max = max.max(score);
+        }
+    }
+    max
+}
+
 fn main() {
     let mut f = std::fs::File::open("input/008.txt").expect("File Error");
     let mut input = String::new();
     f.read_to_string(&mut input).expect("File Read Error");
 
-    println!("there are {} visible trees", visible(&parse_trees(&input)));
+    let orchard = parse_trees(&input);
+    println!("there are {} visible trees", visible(&orchard));
+    println!("the best tree scored {}", best_view(&orchard));
 }
 
 #[cfg(test)]
@@ -100,9 +177,23 @@ mod tests {
         assert_eq!(orchard[0][0], 3);
         assert_eq!(orchard[3][2], 5);
     }
+
     #[test]
     fn test_visibile() {
         let visibile = visible(&parse_trees(SAMPLE));
         assert_eq!(visibile, 21);
+    }
+
+    #[test]
+    fn test_score_location() {
+        let orchard = parse_trees(SAMPLE);
+        assert_eq!(score_location(&orchard, 1, 2), 4);
+        assert_eq!(score_location(&orchard, 3, 2), 8);
+    }
+
+    #[test]
+    fn test_best_location() {
+        let orchard = parse_trees(SAMPLE);
+        assert_eq!(best_view(&orchard), 8);
     }
 }
