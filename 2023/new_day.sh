4bf97e9d863bceb4336795c7f4ddc6d7bd63a9d1
Chris Wren <chris@drwren.com>
2024-01-09 08:55:25 -0500

    add a script to set up a new day

---
+++ 2023/new_day.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+day=$1
+
+tac .vscode/launch.json | sed '1,2d' | tac > .vscode/launch.head
+cat <<EOF >> .vscode/launch.head
+        {
+            "type": "lldb",
+            "request": "launch",
+            "name": "Debug executable '${day}'",
+            "cargo": {
+                "args": [
+                    "build",
+                    "--bin=${day}",
+                    "--package=advent2023"
+                ],
+                "filter": {
+                    "name": "${day}",
+                    "kind": "bin"
+                }
+            },
+            "args": [],
+            "cwd": "\${workspaceFolder}"
+        },
+        {
+            "type": "lldb",
+            "request": "launch",
+            "name": "Debug unit tests in executable '${day}'",
+            "cargo": {
+                "args": [
+                    "test",
+                    "--no-run",
+                    "--bin=${day}",
+                    "--package=advent2023"
+                ],
+                "filter": {
+                    "name": "${day}",
+                    "kind": "bin"
+                }
+            },
+            "args": [],
+            "cwd": "\${workspaceFolder}"
+        },
+    ]
+}
+EOF
+mv .vscode/launch.head .vscode/launch.json 
+cp src/bin/template.rs src/bin/${day}.rs 
+git add src/bin/${day}.rs input/${day}.txt
