4f83438510aef261972008af9ae450cb82cad21a
Chris Wren <chris@drwren.com>
2024-01-12 17:42:07 -0500

    rustfmt 011

--- 2023/src/bin/011.rs
+++ 2023/src/bin/011.rs
@@ -19,12 +19,10 @@ fn find_galaxies(lines: Vec<String>) -> Vec<Coord> {
     galaxies
 }
 
-fn inflate(galaxies:  &Vec<Coord>, expansion_ratio: Integer) -> Vec<Coord> {
+fn inflate(galaxies: &Vec<Coord>, expansion_ratio: Integer) -> Vec<Coord> {
     // inflate in x direction
-    let igroups: HashMap<Integer, Vec<Coord>> = galaxies
-        .to_owned()
-        .into_iter()
-        .into_group_map_by(|a| a.0);
+    let igroups: HashMap<Integer, Vec<Coord>> =
+        galaxies.to_owned().into_iter().into_group_map_by(|a| a.0);
     let mut last = -1;
     let mut num_empty = 0;
     let mut inflated = Vec::new();
@@ -36,10 +34,8 @@ fn inflate(galaxies:  &Vec<Coord>, expansion_ratio: Integer) -> Vec<Coord> {
         }
     }
     // inflate in y direction
-    let jgroups: HashMap<Integer, Vec<Coord>> = inflated
-        .to_owned()
-        .into_iter()
-        .into_group_map_by(|a| a.1);
+    let jgroups: HashMap<Integer, Vec<Coord>> =
+        inflated.to_owned().into_iter().into_group_map_by(|a| a.1);
     inflated.clear();
     let mut last = -1;
     let mut num_empty = 0;
@@ -53,11 +49,11 @@ fn inflate(galaxies:  &Vec<Coord>, expansion_ratio: Integer) -> Vec<Coord> {
     inflated
 }
 
-fn compute_distances(galaxies:  &Vec<Coord>) -> Vec<Integer> {
+fn compute_distances(galaxies: &Vec<Coord>) -> Vec<Integer> {
     let mut distances = Vec::new();
-    for (i , a) in galaxies.iter().enumerate() {
+    for (i, a) in galaxies.iter().enumerate() {
         for b in galaxies.iter().skip(i + 1) {
-            distances.push((a.0-b.0).abs() + (a.1-b.1).abs());
+            distances.push((a.0 - b.0).abs() + (a.1 - b.1).abs());
         }
     }
     distances
@@ -73,11 +69,17 @@ fn main() {
     let galaxies = find_galaxies(lines);
     let inflated = inflate(&galaxies, 1);
     let distances = compute_distances(&inflated);
-    println!("sum of intergalactic ditances is {}", distances.iter().sum::<Integer>());
+    println!(
+        "sum of intergalactic ditances is {}",
+        distances.iter().sum::<Integer>()
+    );
 
     let inflated = inflate(&galaxies, 999999);
     let distances = compute_distances(&inflated);
-    println!("sum of hyper-inflated ditances is {}", distances.iter().sum::<Integer>());
+    println!(
+        "sum of hyper-inflated ditances is {}",
+        distances.iter().sum::<Integer>()
+    );
 }
 
 #[cfg(test)]
@@ -110,25 +112,34 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let lines = CONTRACTED.lines().map(|s| s.to_string()).collect::<Vec<_>>();
+        let lines = CONTRACTED
+            .lines()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
         let galaxies = find_galaxies(lines);
         assert_eq!(galaxies.len(), 9);
-        assert_eq!(galaxies, vec![
-            (3, 0),
-            (7, 1,),
-            (0, 2),
-            (6, 4),
-            (1, 5),
-            (9, 6),
-            (7, 8),
-            (0, 9),
-            (4, 9),
-        ]);
+        assert_eq!(
+            galaxies,
+            vec![
+                (3, 0),
+                (7, 1,),
+                (0, 2),
+                (6, 4),
+                (1, 5),
+                (9, 6),
+                (7, 8),
+                (0, 9),
+                (4, 9),
+            ]
+        );
     }
 
     #[test]
     fn test_expand() {
-        let lines = CONTRACTED.lines().map(|s| s.to_string()).collect::<Vec<_>>();
+        let lines = CONTRACTED
+            .lines()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
         let galaxies = find_galaxies(lines);
         let lines = EXPANDED.lines().map(|s| s.to_string()).collect::<Vec<_>>();
         let expected = find_galaxies(lines);
@@ -138,7 +149,10 @@ mod tests {
 
     #[test]
     fn test_distances() {
-        let lines = CONTRACTED.lines().map(|s| s.to_string()).collect::<Vec<_>>();
+        let lines = CONTRACTED
+            .lines()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
         let galaxies = find_galaxies(lines);
         let inflated = inflate(&galaxies, 1);
         let distances = compute_distances(&inflated);
@@ -148,7 +162,10 @@ mod tests {
 
     #[test]
     fn test_rapid_inflation() {
-        let lines = CONTRACTED.lines().map(|s| s.to_string()).collect::<Vec<_>>();
+        let lines = CONTRACTED
+            .lines()
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
         let galaxies = find_galaxies(lines);
 
         let inflated = inflate(&galaxies, 9);
